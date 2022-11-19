2e34c5afe6f4bb8242ddff93e8402e58eb060a2f
HayaUp <hayaup.git@gmail.com>
2022-11-20 01:12:15 +0900

    プロジェクト ファイルを追加します。

---
+++ KazuateGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KazuateGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
